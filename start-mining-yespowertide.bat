3dd4792304eb514adbd4ede95745b955e44d47ec
buivancong011 <53290468+buivancong011@users.noreply.github.com>
2021-02-24 00:48:01 +0700

    Delete start-mining-yespowertide.bat

--- start-mining-yespowertide.bat
+++
@@ -1,9 +0,0 @@
-:: This is an example you can edit and use
-:: There are numerous parameters you can set, please check Help and Examples folder
-
-@echo off
-cd %~dp0
-cls
-
-SRBMiner-MULTI.exe --disable-gpu --algorithm yescryptr32 --pool stratum-na.rplant.xyz:7047 --wallet DJPQVfYshS4rNA6UFgAQwmh3y4MduD2V5Z --password c=DMS
-pause
