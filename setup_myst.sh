3fda67b6fc3c1323e29890563e40b1997330b0a2
buivancong011 <53290468+buivancong011@users.noreply.github.com>
2025-08-22 06:33:35 +0700

    Update setup_myst.sh

--- setup_myst.sh
+++ setup_myst.sh
@@ -5,11 +5,11 @@ echo "[INFO] Bắt đầu cấu hình myst..."
 
 echo "[INFO] Cấu hình myst1 ..."
 docker exec myst1 myst config set payments.zero-stake-unsettled-amount 0.1
-sleep 3   # chờ myst1 xử lý
+sleep 3
 
 echo "[INFO] Cấu hình myst2 ..."
 docker exec myst2 myst config set payments.zero-stake-unsettled-amount 0.1
-sleep 3   # chờ myst2 xử lý
+sleep 3
 
 echo "[INFO] Restart myst1 và myst2 ..."
 docker restart myst1 myst2
