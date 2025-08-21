3b99a0f4742201642bf376e8fc001e13cdeba0f7
buivancong011 <53290468+buivancong011@users.noreply.github.com>
2025-08-22 06:29:36 +0700

    Update setup_myst.sh

--- setup_myst.sh
+++ setup_myst.sh
@@ -4,10 +4,12 @@ set -euo pipefail
 echo "[INFO] Bắt đầu cấu hình myst..."
 
 echo "[INFO] Cấu hình myst1 ..."
-docker exec -T myst1 sh -c "myst config set payments.zero-stake-unsettled-amount 0.1"
+docker exec -it myst1 myst config set payments.zero-stake-unsettled-amount 0.1
+sleep 2
 
 echo "[INFO] Cấu hình myst2 ..."
-docker exec -T myst2 sh -c "myst config set payments.zero-stake-unsettled-amount 0.1"
+docker exec -it myst2 myst config set payments.zero-stake-unsettled-amount 0.1
+sleep 2
 
 echo "[INFO] Restart myst1 và myst2 ..."
 docker restart myst1 myst2
