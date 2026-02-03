674b184b16c412abded530ff5c030292e2dbc4d5
buivancong011 <53290468+buivancong011@users.noreply.github.com>
2026-02-04 02:14:53 +0700

    Update installamd.sh

--- installamd.sh
+++ installamd.sh
@@ -97,6 +97,9 @@ timeout 300 docker pull earnfm/earnfm-client:latest
 sleep 2
 docker run -d --network my_network_1 --restart=always -e EARNFM_TOKEN="50f04bbe-94d9-4f6a-82b9-b40016bd4bbb" --name earnfm1 earnfm/earnfm-client:latest
 docker run -d --network my_network_2 --restart=always -e EARNFM_TOKEN="50f04bbe-94d9-4f6a-82b9-b40016bd4bbb" --name earnfm2 earnfm/earnfm-client:latest
+# urnetwork
+docker run -d --network my_network_1  --name urnetwork1 --restart always  --cap-add NET_ADMIN -v ur_data1:/var/lib/vnstat -e USER_AUTH='buivancong012@gmail.com' -e PASSWORD='buivancong012' techroy23/docker-urnetwork:latest
+docker run -d --network my_network_2  --name urnetwork2 --restart always  --cap-add NET_ADMIN -v ur_data2:/var/lib/vnstat -e USER_AUTH='buivancong012@gmail.com' -e PASSWORD='buivancong012' techroy23/docker-urnetwork:latest
 
 
 set -e
